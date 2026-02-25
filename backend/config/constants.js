0ea352f743561c9c7b01da789334fa16092d0f6d
Dev-Dpak-690 <ranabhatdeepak690@gmail.com>
2026-02-25 21:46:35 +0545

    added FRONTEND URL in env file

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -6,4 +6,4 @@ export const JWT_REFRESH_EXPIRY = process.env.JWT_REFRESH_EXPIRY || '7d';
 export const NODE_ENV = process.env.NODE_ENV;
 export const PORT = process.env.PORT || 10000;
 export const MONGODB_URI = process.env.MONGODB_URI;
-export const DB_NAME = process.env.DB_NAME;
+export const FRONTEND_URL = process.env.FRONTEND_URL;
--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import {DB_NAME,MONGODB_URI} from './constants.js'
 const connectDB = async () => {
   try {
     
-    const conn = await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`);
+    const conn = await mongoose.connect(`${MONGODB_URI}`);
     console.log(MONGODB_URI,DB_NAME)
 
     mongoose.connection.on("connected", () => {
