36c8c7577bfb94d444e6e5686f01ce74c904e049
Dev-Dpak-690 <ranabhatdeepak690@gmail.com>
2026-02-25 20:00:43 +0545

    add node server.js in script of package.json

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,34 @@
-import 'dotenv/config'
-import express from 'express';
-import cors from 'cors'
-import connectDB from './config/db.js';
-import errorHandler from './middlewares/error.middleware.js';
-import cookieParser from 'cookie-parser';
-import AppError from './utils/AppError.js';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import errorHandler from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
+import AppError from "./utils/AppError.js";
 connectDB();
 
-
-{/* Routes */}
-import authRoutes from './routes/auth.route.js';
-import friendRoutes from './routes/friend.route.js';
-import chatRoutes from './routes/chat.route.js'
+{
+  /* Routes */
+}
+import authRoutes from "./routes/auth.route.js";
+import friendRoutes from "./routes/friend.route.js";
+import chatRoutes from "./routes/chat.route.js";
 const app = express();
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true,
-  methods:["GET","POST","PUT","PATCH","DELETE"]
-}))
+app.use(
+  cors({
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.get("/health", (req, res) => res.sendStatus(200));
 
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/friend", friendRoutes);
+app.use("/api/v1/chat", chatRoutes);
 
-app.use('/api/v1/auth',authRoutes);
-app.use('/api/v1/friend',friendRoutes);
-app.use('/api/v1/chat',chatRoutes)
-
-
-app.all('/{*path}', (req, res, next) => {
+app.all("/{*path}", (req, res, next) => {
   next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
 });
 app.use(errorHandler);
